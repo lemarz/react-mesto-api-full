d61a872e071c0a2aa5a0facb59890567e9a6986f
Leonid Marz <l3m4rz@gmail.com>
2023-01-25 20:18:45 +0300

    feat: добавить cors

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const cors = require('cors');
 const { registerJoiValidator, loginJoiValidator } = require('./middlewares/joiValidator');
 const auth = require('./middlewares/auth');
 const notFound = require('./middlewares/notFound');
@@ -14,7 +13,31 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(cors());
+const allowedCors = [
+  'http://localhost:3000',
+  'http://localhost:4000',
+  'https://lastone.nomoredomains.club',
+  'http://lastone.nomoredomains.club',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
